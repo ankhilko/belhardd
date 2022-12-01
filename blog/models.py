e9ec90392f24ec3bd949e0a19693e0e3a9491f1e
sanr <fieldstreet.27@gmail.com>
2022-12-01 19:52:08 +0300

    starting django day 1

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,40 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Category(models.Model):
+    name = models.CharField(
+        max_length=64,
+        null=False,
+        blank=False,
+        unique=True,
+        verbose_name='название',
+        help_text='Не более 64 символов',
+    )
+    is_published = models.BooleanField(
+        default=True,
+        verbose_name='публикация',
+    )
+    slug = models.SlugField(
+        verbose_name='URL',
+        unique=True
+    )
+
+
+    def __str__(self):
+        return self.name
+
+
+    class Meta:
+        verbose_name = 'категория'
+        verbose_name_plural = 'категории'
+        db_table = 'blog_categories'
+
+
+
+class Post:
+    name
+    url
+
+    pass
